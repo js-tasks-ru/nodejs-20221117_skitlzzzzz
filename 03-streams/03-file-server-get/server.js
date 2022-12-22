578dbed59aad35d5df4416946215de0e77050753
Дарья <yppix@yandex.ru>
2022-12-22 20:34:23 +0300

    fxs hw-3 except 2

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,15 +11,36 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const stream = fs.createReadStream(filepath);
+  stream.pipe(res);
+
+  stream.on('open', () => console.log('open'));
+  stream.on('end', () => console.log('end'));
+
   switch (req.method) {
     case 'GET':
-
+      if (error.code === 'ENOENT') {
+        if (pathname.includes('/')) {
+          res.statusCode = 400;
+          res.end('incorrect req');
+        } else {
+          res.statusCode = 404;
+          res.end('no such file');
+        }
+      } else {
+        res.statusCode = 500;
+        res.end('internal server error');
+      }
       break;
 
     default:
       res.statusCode = 501;
       res.end('Not implemented');
   }
+
+  req.on('abort', () => {
+    stream.destroy();
+  });
 });
 
 module.exports = server;
