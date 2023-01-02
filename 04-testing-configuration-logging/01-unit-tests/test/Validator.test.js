60f5ebffa80165d57c7b7eeb84ff4ec122b9f7cc
Дарья <yppix@yandex.ru>
2023-01-03 01:30:26 +0300

    hw-4

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -12,11 +12,83 @@ describe('testing-configuration-logging/unit-tests', () => {
         },
       });
 
-      const errors = validator.validate({ name: 'Lalala' });
+      const errors = validator.validate({name: 'Lalala'});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
       expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
+    });
+  });
+
+  describe('Validator 2', () => {
+    it('валидатор проверяет числовые поля', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 18,
+          max: 27,
+        },
+      });
+
+      const errors = validator.validate({age: 3});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too little, expect 18, got 3');
+    });
+  });
+
+  describe('Validator 3', () => {
+    it('валидатор проверяет соответствие типов числовых полей', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 18,
+          max: 27,
+        },
+      });
+
+      const errors = validator.validate({age: true});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect number, got boolean');
+    });
+  });
+
+  describe('Validator 4', () => {
+    it('валидатор проверяет соответствие типов строковых полей', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 18,
+          max: 27,
+        },
+      });
+
+      const errors = validator.validate({name: true});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect string, got boolean');
+    });
+  });
+
+  describe('Validator 4', () => {
+    it('валидатор проверяет пустоты', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 18,
+          max: 27,
+        },
+      });
+
+      const errors = validator.validate({});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect params, got null');
     });
   });
 });
