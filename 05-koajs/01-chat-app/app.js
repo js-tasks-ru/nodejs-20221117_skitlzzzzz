100132a27803b4ba9603ed6a6215425b6b94ad08
Дарья <yppix@yandex.ru>
2023-01-06 03:45:30 +0300

    hw-5

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -7,11 +7,28 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const clients = new Set();
 
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve, reject)=>{
+    clients.add(resolve);
+  });
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (!message) {
+    ctx.throw(400, 'need message');
+  }
+
+  clients.forEach(function(resolve) {
+    resolve(message);
+  });
+
+  clients.clear();
+
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
