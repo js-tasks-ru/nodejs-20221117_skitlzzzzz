334d6c41eb7c6df408793e1cb6b4829803b7746b
Дарья <yppix@yandex.ru>
2023-01-11 15:52:52 +0300

    hw-6-1

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: 'Название подкатегории должно быть',
+  },
 });
 
 const categorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: 'Название категории должно быть',
+  },
+  subcategories: {
+    type: [subCategorySchema],
+  },
 });
 
 module.exports = connection.model('Category', categorySchema);
--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -2,6 +2,31 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: 'Название товара должно быть',
+  },
+  description: {
+    type: String,
+    required: 'Описание должно быть',
+  },
+  price: {
+    type: Number,
+    required: 'Цена должно быть',
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: 'Название категории должно быть',
+  },
+  subcategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category.Subcategory',
+    required: 'Название подкатегории должно быть',
+  },
+  images: {
+    type: [String],
+  },
 });
 
 module.exports = connection.model('Product', productSchema);
