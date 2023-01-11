cc0c115dbe3e1261be35cab4242a6a1895cf99bc
Дарья <yppix@yandex.ru>
2023-01-11 21:55:34 +0300

    hw-6-2

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose');
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  if (!mongoose.isValidObjectId(ctx.params.id)) {
+    ctx.throw(40, 'category not found');
+  }
+
+  const category = await Category.findById(ctx.params.id);
+
+  if (!category) {
+    ctx.throw(404, 'category not found');
+  }
+
+  ctx.body = {categories: category.populate('subcategory')};
 };
--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,39 @@
+const mongoose = require('mongoose');
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const products = await Product.find({subcategory: subcategory});
+
+  if (!products) {
+    ctx.throw(404, 'no products in subcategory');
+  }
+
+  ctx.body = {products: []};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = Product.find();
+
+  if (!products) {
+    ctx.throw(404, 'product not found');
+  }
+
+  ctx.body = products;
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if (!mongoose.isValidObjectId(ctx.params.id)) {
+    ctx.throw(404, 'product not found');
+  }
+
+  const products = await Product.findById(ctx.params.id);
+
+  if (!products) {
+    ctx.throw(404, 'product not found');
+  }
+
+  ctx.body = products;
 };
 
